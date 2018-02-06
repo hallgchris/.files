3af01c603c65f28ee535b0620d688a550d3770f6
Christopher Hall <christopher.hall.3609@gmail.com>
2018-02-06 21:29:50 +1100

    Polybar config through enviroment variables for stuff that varies per computer

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,8 @@ alias vim="nvim"
 set PATH ~/.scripts $PATH
 set XDG_CONFIG_HOME ~/.config
 
+posix-source ~/.config/fish/env
+
 # Start X at login
 if status --is-login
     if test -z "$DISPLAY" -a $XDG_VTNR = 1
---
+++ fish/.config/fish/functions/posix-source.fish
@@ -0,0 +1,8 @@
+# lewandowski.io/2016/10/fish-env/
+function posix-source
+	for i in (cat $argv)
+		set arr (echo $i |tr = \n)
+		set -gx $arr[1] $arr[2]
+	end
+end
+

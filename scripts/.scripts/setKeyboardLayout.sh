cbeb618760057a257e7e05c1f58ab800bf15e5c9
Christopher Hall <christopher.hall.3609@gmail.com>
2018-06-17 15:51:53 +1000

    Script for keyboard layout stuff

---
+++ scripts/.scripts/setKeyboardLayout.sh
@@ -0,0 +1,5 @@
+# Only needs to be run once, but saving it here so I don't have to spend an hour working it out again
+
+sudo localectl set-keymap colemak
+sudo localectl --no-convert set-x11-keymap "us(colemak),us" pc105 "" shift:both_capslock,grp:alt_shift_toggle
+
